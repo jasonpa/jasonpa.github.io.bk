20c92a05e3c905b7d5c2715d85fc9e39cead9e93
Jiman Ryu <jiman.ryu@gmail.com>
2020-07-27 14:18:42 -0700

    Updating to jasonpa/rc.v2@b5dc39fbc5fe600de0750eee0b69789fb09d7b8a.

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,91 @@
+var store = [{
+        "title": "Self Care After AcuPoint Injections",
+        "excerpt":"Sections in this article What changes have occurred in my body as a result of the injections? What will the discomfort or pain be like afterwards? How long will post-injection soreness last? What should I do to ease discomfort after injections? Will moist heat help? Will ice help? Will massaging...","categories": ["Resources"],
+        "tags": ["Resources","Treatment"],
+        "url": "https://ryu.clinic/resources/self-care-after-acupoint-injections/",
+        "teaser": "https://ryu.clinic/assets/images/natasha-spencer-_hH0dC6A-FM-unsplash.jpg"
+      },{
+        "title": "Acupoint Injection Overview",
+        "excerpt":"Sections in this article Acupoint Injection Overview Acupoint Injection Overview According to famous TCM author Qiu Maoliang‘s most influential textbook, Chinese Acupuncture and Moxibustion; acupoint injection therapy is described as …a method of injecting liquid medicine into acupoints in order to prevent and treat disease. It combines permeating and stimulating...","categories": ["Treatment"],
+        "tags": ["Getting started","Treatment","Overview"],
+        "url": "https://ryu.clinic/treatment/acupoint-injection-overview/",
+        "teaser": "https://ryu.clinic/assets/images/hyttalo-souza-a1p0Z7RSkL8-unsplash.jpg"
+      },{
+        "title": "Prolotherapy Overview",
+        "excerpt":"Sections in this article Prolotherapy Overview The first stage is inflammation The second stage is proliferation The third stage is the remodeling phase Relevance to clinical outcomes References Prolotherapy Overview Prolotherapy, also called proliferation therapy or regenerative injection therapy is a treatment of tissue with the injection of an irritant...","categories": ["Treatment"],
+        "tags": ["Getting started","Treatment","Overview"],
+        "url": "https://ryu.clinic/treatment/prolotherapy-overview/",
+        "teaser": "https://ryu.clinic/assets/images/ariabc.org.png"
+      },{
+        "title": "What Is Community Acupuncture?",
+        "excerpt":"Sections in this article The Short Version For The Long Version, read here How can you afford to charge so little to community acupuncture? Do I have to take my clothes off for community acupuncture treatment? References The Short Version 1 We define Community Acupuncture as the practice of offering...","categories": [],
+        "tags": ["Getting started"],
+        "url": "https://ryu.clinic/what-is-community-acupuncture/",
+        "teaser": "https://ryu.clinic/assets/images/ariabc.org.png"
+      },{
+        "title": "How to Prepare For and Receive Acupuncture",
+        "excerpt":"Here is a good article for prepare and receive acupuncture from Wiki How It’s likely that you have heard of acupuncture, and you may even know someone who has experienced it. Acupuncture is the attempt to restore and maintain health through the stimulation of specific anatomical points on the body....","categories": [],
+        "tags": ["Getting started"],
+        "url": "https://ryu.clinic/how-to-prepare-for-and-receive-acupuncture/",
+        "teaser": "https://ryu.clinic/assets/images/clem-onojeghuo-zlABb6Gke24-unsplash.jpg"
+      },{
+        "title": "Treatment Method",
+        "excerpt":"Acupoint Injection Therapy Auricular Acupuncture The ear is a microsystem which reflects the entire body. Conditions affecting the physical, mental or emotional health of the patient can be treatable by stimulating the surface of the ear with a small seed. It is also called auricular acupuncture. Cupping Cupping is a...","categories": ["Treatment"],
+        "tags": ["Getting started","Treatment"],
+        "url": "https://ryu.clinic/treatment/treatment-method/",
+        "teaser": null
+      },{
+        "title": "Underground Free Parking",
+        "excerpt":"We have 1.5 hours free underground parking. Parking for all patrons on the P1 level of the parkade will be free for the first 90 minutes, but will require vehicle registration at the various metres installed around the site, or by using Impark’s Hangtag App. Pay parking will be in...","categories": [],
+        "tags": ["About"],
+        "url": "https://ryu.clinic/underground-free-parking/",
+        "teaser": null
+      },{
+        "title": "2018 Summer Holiday Closing Notice",
+        "excerpt":"Please be informed that we will be closed from July 23 to 28 due to the Summer Holiday Closing. Inquiries during holidays will be reply after July 30. Thank you for your understanding and continued support with us.  ","categories": [],
+        "tags": ["News"],
+        "url": "https://ryu.clinic/2018-summer-holiday-notice/",
+        "teaser": null
+      },{
+        "title": "Why is pain killer not effective for chronic pain?",
+        "excerpt":"Sections in this article Overview How is chronic pain classified? Treatment of chronic pain with Herbal medicine References Overview As you can see this article There’s a chronic pain crisis in Canada, and governments must address it, chronic pain affects one in every five Canadians, or six million of us....","categories": [],
+        "tags": ["Getting started"],
+        "url": "https://ryu.clinic/why-is-pain-killer-not-effective-for-chronic-pain/",
+        "teaser": null
+      },{
+        "title": "ICBC 교통사고 치료",
+        "excerpt":"2019년 4월 1일부로 바뀐 ICBC 정책 2019년 4월 1일 부터 ICBC 교통사고 치료관련 제도가 바뀝니다.1 2 사고가 나면 가장 중요한 것이 만성으로 넘어가지 않도록 빠른 시간안에 치료를 시작하는 것입니다. 앞으로는 번거로운 사전 과정 없이 클레임 넘버만 가지고 클리닉으로 오시면 바로 한방치료를 시작하실 수 있습니다. 사고가 난 날로부터 12주 이내에는 Minor...","categories": [],
+        "tags": ["News"],
+        "url": "https://ryu.clinic/icbc-%EA%B5%90%ED%86%B5%EC%82%AC%EA%B3%A0%EC%B9%98%EB%A3%8C/",
+        "teaser": "https://ryu.clinic/assets/images/icbc_logo.svg"
+      },{
+        "title": "무료 미술 힐링강의 (칼라와 에너지)",
+        "excerpt":"무료 미술 힐링강의 (칼라와 에너지) 1 “칼라와 그림이 우리에게 얼마나 큰 영향력을 갖고 있는지 알아보는 시간입니다.” “언제 사랑의 감정을 느껴보셨습니까?” 새로운 그림의 세계를 알게 되는 경이로움과 충만함, 그리고 그림과 나누는 포근한 사랑의 시간이 앞으로 계속 밴쿠버에서 이어집니다. 강의 주제는 계속 바뀝니다. 그림과 파동 그림과 감정이 몸에 미치는 영향 그림에 담긴...","categories": [],
+        "tags": ["News"],
+        "url": "https://ryu.clinic/healing-arts-seminar/",
+        "teaser": "https://ryu.clinic/assets/images/event-1.jpg"
+      },{
+        "title": "Enjoy the Labour Day long weekend",
+        "excerpt":"In observance of Labor Day, our office will be closed on Monday (Sep.2)  ","categories": [],
+        "tags": ["News"],
+        "url": "https://ryu.clinic/2018-labour-day-notice/",
+        "teaser": "https://ryu.clinic/assets/images/vince-fleming-7qileFwJEWo-unsplash.jpg"
+      },{
+        "title": "You Deserve It",
+        "excerpt":"This is a figure caption. Do something nice for yourself today. You deserve a break. Self-care is how you take your power back. There are lots of possible explanation for benefit of acupuncture and herbal medicine. But the people are not interested in the explanation of the medical approach. We...","categories": [],
+        "tags": ["News"],
+        "url": "https://ryu.clinic/you-deserve-it/",
+        "teaser": "https://ryu.clinic/assets/images/mareko-tamaleaa-u29A-hrVgJU-unsplash.jpg"
+      },{
+        "title": "ICBC Motor Vehicle Accident Assessment and Treatment",
+        "excerpt":"ICBC policy changed as of April 1, 2019 2019년 4월 1일 부터 ICBC 교통사고 치료관련 제도가 바뀝니다.1 2 사고가 나면 가장 중요한 것이 만성으로 넘어가지 않도록 빠른 시간안에 치료를 시작하는 것입니다. 앞으로는 번거로운 사전 과정 없이 클레임 넘버만 가지고 클리닉으로 오시면 바로 한방치료를 시작하실 수 있습니다. 사고가 난 날로부터 12주...","categories": [],
+        "tags": ["About","News"],
+        "url": "https://ryu.clinic/icbc-motor-vehicle-accident-assessment-and-treatment/",
+        "teaser": "https://ryu.clinic/assets/images/icbc_logo.svg"
+      },{
+        "title": "2019 Suter Brook Winter Festival",
+        "excerpt":"We are sponsoring the 2019 Suter Brook Winter Festival.   Take a ride on the horse-drawn carriage, interact with festive mascots, grab a photo with Mr.&amp;Mrs.Claus, and enjoy live ice carving and holiday music!   Date: Dec.5th 2019 4 pm - 7 pm   Location: Suter Brook Village at Port Moody     ","categories": [],
+        "tags": ["News"],
+        "url": "https://ryu.clinic/2019-suter-brook-winter-festival/",
+        "teaser": "https://ryu.clinic/assets/images/2019-winter-festival.jpg"
+      }]
