3049188a7d5fe918fc8792636ec575e1b1a81c89
Jiman Ryu <jiman.ryu@gmail.com>
2020-08-05 15:29:09 -0700

    Updating to jasonpa/rc.v2@ada6370e5248424f53fd74787e9d9fb35543cbbc.

---
+++ ko/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "ICBC 교통사고 치료",
+        "excerpt":"2019년 4월 1일부로 바뀐 ICBC 정책 2019년 4월 1일 부터 ICBC 교통사고 치료관련 제도가 바뀝니다.1 2 사고가 나면 가장 중요한 것이 만성으로 넘어가지 않도록 빠른 시간안에 치료를 시작하는 것입니다. 앞으로는 번거로운 사전 과정 없이 클레임 넘버만 가지고 클리닉으로 오시면 바로 한방치료를 시작하실 수 있습니다. 사고가 난 날로부터 12주 이내에는 Minor...","categories": [],
+        "tags": ["News"],
+        "url": "https://ryu.clinic/ko/icbc-%EA%B5%90%ED%86%B5%EC%82%AC%EA%B3%A0%EC%B9%98%EB%A3%8C/",
+        "teaser": "https://ryu.clinic/ko/assets/images/icbc_logo.svg"
+      },{
+        "title": "ICBC Motor Vehicle Accident Assessment and Treatment",
+        "excerpt":"ICBC policy changed as of April 1, 2019 2019년 4월 1일 부터 ICBC 교통사고 치료관련 제도가 바뀝니다.1 2 사고가 나면 가장 중요한 것이 만성으로 넘어가지 않도록 빠른 시간안에 치료를 시작하는 것입니다. 앞으로는 번거로운 사전 과정 없이 클레임 넘버만 가지고 클리닉으로 오시면 바로 한방치료를 시작하실 수 있습니다. 사고가 난 날로부터 12주...","categories": [],
+        "tags": ["About","News"],
+        "url": "https://ryu.clinic/ko/icbc-motor-vehicle-accident-assessment-and-treatment/",
+        "teaser": "https://ryu.clinic/ko/assets/images/icbc_logo.svg"
+      }]
