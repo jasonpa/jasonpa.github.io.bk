aac62edbfe8aee2b0f4306c37f872f70d012c9f0
Jiman Ryu <jiman.ryu@gmail.com>
2022-10-26 17:57:34 +0000

    Updating to jasonpa/rc.v2@49ad84957735fdedeb4c91c61e74ed512336d6bd.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,96 +2,96 @@ var store = [{
         "title": "Self Care After AcuPoint Injections",
         "excerpt":"You have been given trigger point injections today. This information reviews what you can expect to feel, and reminds you of important things to do during the next few days in order to minimize your discomfort and get the best results possible from the treatment. What changes have occurred in...","categories": ["Resources"],
         "tags": ["Resources","Treatment"],
-        "url": "https://ryu.clinic/resources/self-care-after-acupoint-injections/",
-        "teaser": "https://ryu.clinic/assets/images/natasha-spencer-_hH0dC6A-FM-unsplash.jpg"
+        "url": "/resources/self-care-after-acupoint-injections/",
+        "teaser": "/assets/images/natasha-spencer-_hH0dC6A-FM-unsplash.jpg"
       },{
         "title": "Acupoint Injection Overview",
         "excerpt":"Acupoint Injection Overview According to famous TCM author Qiu Maoliang‘s most influential textbook, Chinese Acupuncture and Moxibustion; acupoint injection therapy is described as …a method of injecting liquid medicine into acupoints in order to prevent and treat disease. It combines permeating and stimulating functions of needles and medicine, and can...","categories": ["Treatment"],
         "tags": ["Getting started","Treatment","Overview"],
-        "url": "https://ryu.clinic/treatment/acupoint-injection-overview/",
+        "url": "/treatment/acupoint-injection-overview/",
         "teaser": null
       },{
         "title": "Prolotherapy Overview",
         "excerpt":"Prolotherapy, also called proliferation therapy or regenerative injection therapy is a treatment of tissue with the injection of an irritant solution into a joint space, weakened ligament, or tendon insertion to relieve pain. The term Proli is Latin for “to grow.” The most commonly injected irritant solution used is dextrose,...","categories": ["Treatment"],
         "tags": ["Getting started","Treatment","Overview"],
-        "url": "https://ryu.clinic/treatment/prolotherapy-overview/",
-        "teaser": "https://ryu.clinic/assets/images/ariabc.org.png"
+        "url": "/treatment/prolotherapy-overview/",
+        "teaser": "/assets/images/ariabc.org.png"
       },{
         "title": "What Is Community Acupuncture?",
         "excerpt":"Sections in this article The Short Version For The Long Version, read here How can you afford to charge so little to community acupuncture? Do I have to take my clothes off for community acupuncture treatment? References The Short Version 1 We define Community Acupuncture as the practice of offering...","categories": [],
         "tags": ["Getting started"],
-        "url": "https://ryu.clinic/what-is-community-acupuncture/",
-        "teaser": "https://ryu.clinic/assets/images/ariabc.org.png"
+        "url": "/what-is-community-acupuncture/",
+        "teaser": "/assets/images/ariabc.org.png"
       },{
         "title": "How to Prepare For and Receive Acupuncture",
         "excerpt":"Here is a good article for prepare and receive acupuncture from Wiki How It’s likely that you have heard of acupuncture, and you may even know someone who has experienced it. Acupuncture is the attempt to restore and maintain health through the stimulation of specific anatomical points on the body....","categories": [],
         "tags": ["Getting started"],
-        "url": "https://ryu.clinic/how-to-prepare-for-and-receive-acupuncture/",
-        "teaser": "https://ryu.clinic/assets/images/clem-onojeghuo-zlABb6Gke24-unsplash.jpg"
+        "url": "/how-to-prepare-for-and-receive-acupuncture/",
+        "teaser": "/assets/images/clem-onojeghuo-zlABb6Gke24-unsplash.jpg"
       },{
         "title": "Treatment Method",
         "excerpt":"Auricular Acupuncture The ear is a microsystem which reflects the entire body. Conditions affecting the physical, mental or emotional health of the patient can be treatable by stimulating the surface of the ear with a small seed. It is also called auricular acupuncture. Cupping Cupping is a therapy designed to...","categories": ["Treatment"],
         "tags": ["Getting started","Treatment"],
-        "url": "https://ryu.clinic/treatment/treatment-method/",
+        "url": "/treatment/treatment-method/",
         "teaser": null
       },{
         "title": "Underground Free Parking",
         "excerpt":"We have 1.5 hours free underground parking. Parking for all patrons on the P1 level of the parkade will be free for the first 90 minutes, but will require vehicle registration at the various metres installed around the site, or by using Impark’s Hangtag App. Pay parking will be in...","categories": [],
         "tags": ["About"],
-        "url": "https://ryu.clinic/underground-free-parking/",
+        "url": "/underground-free-parking/",
         "teaser": null
       },{
         "title": "2018 Summer Holiday Closing Notice",
         "excerpt":"Please be informed that we will be closed from July 23 to 28 due to the Summer Holiday Closing. Inquiries during holidays will be reply after July 30. Thank you for your understanding and continued support with us.  ","categories": [],
         "tags": ["News"],
-        "url": "https://ryu.clinic/2018-summer-holiday-notice/",
+        "url": "/2018-summer-holiday-notice/",
         "teaser": null
       },{
         "title": "Why is pain killer not effective for chronic pain?",
         "excerpt":"Sections in this article Overview How is chronic pain classified? Treatment of chronic pain with Herbal medicine References Overview As you can see this article There’s a chronic pain crisis in Canada, and governments must address it, chronic pain affects one in every five Canadians, or six million of us....","categories": [],
         "tags": ["Getting started"],
-        "url": "https://ryu.clinic/why-is-pain-killer-not-effective-for-chronic-pain/",
+        "url": "/why-is-pain-killer-not-effective-for-chronic-pain/",
         "teaser": null
       },{
         "title": "Enjoy the Labour Day long weekend",
         "excerpt":"In observance of Labor Day, our office will be closed on Monday (Sep.2)  ","categories": [],
         "tags": ["News"],
-        "url": "https://ryu.clinic/2018-labour-day-notice/",
-        "teaser": "https://ryu.clinic/assets/images/vince-fleming-7qileFwJEWo-unsplash.jpg"
+        "url": "/2018-labour-day-notice/",
+        "teaser": "/assets/images/vince-fleming-7qileFwJEWo-unsplash.jpg"
       },{
         "title": "You Deserve It",
         "excerpt":"This is a figure caption. Do something nice for yourself today. You deserve a break. Self-care is how you take your power back. There are lots of possible explanation for benefit of acupuncture and herbal medicine. But the people are not interested in the explanation of the medical approach. We...","categories": [],
         "tags": ["News"],
-        "url": "https://ryu.clinic/you-deserve-it/",
-        "teaser": "https://ryu.clinic/assets/images/mareko-tamaleaa-u29A-hrVgJU-unsplash.jpg"
+        "url": "/you-deserve-it/",
+        "teaser": "/assets/images/mareko-tamaleaa-u29A-hrVgJU-unsplash.jpg"
       },{
         "title": "ICBC Motor Vehicle Accident Assessment and Treatment",
         "excerpt":"We do Direct Billing ICBC 1 No doctor's referral required 2 No upfront payment required 3 No extra charges at our clinic ICBC policy changed as of April 1, 2019 From April 1, 2019, the ICBC ICBC Motor Vehicle Accident (MVA) Treatment System will be changed.1 2 In the future,...","categories": [],
         "tags": ["About","News"],
-        "url": "https://ryu.clinic/icbc-motor-vehicle-accident-assessment-and-treatment/",
-        "teaser": "https://ryu.clinic/assets/images/icbc_logo.svg"
+        "url": "/icbc-motor-vehicle-accident-assessment-and-treatment/",
+        "teaser": "/assets/images/icbc_logo.svg"
       },{
         "title": "2019 Suter Brook Winter Festival",
         "excerpt":"We are sponsoring the 2019 Suter Brook Winter Festival.   Take a ride on the horse-drawn carriage, interact with festive mascots, grab a photo with Mr.&amp;Mrs.Claus, and enjoy live ice carving and holiday music!   Date: Dec.5th 2019 4 pm - 7 pm   Location: Suter Brook Village at Port Moody     ","categories": [],
         "tags": ["News"],
-        "url": "https://ryu.clinic/2019-suter-brook-winter-festival/",
-        "teaser": "https://ryu.clinic/assets/images/2019-winter-festival.jpg"
+        "url": "/2019-suter-brook-winter-festival/",
+        "teaser": "/assets/images/2019-winter-festival.jpg"
       },{
         "title": "Welcome to our new RMT Oliviah",
         "excerpt":"Oliviah, R.Ac, R.M.T. She has been practicing many years of experience in Acupuncture. Recently she got a new license as a Registered Massage Therapist. She is registered in CMTBC1. She has a passion for massage, Tui-na, and Acupressure before so she decided to add a new title. She understands different conditions...","categories": [],
         "tags": ["News"],
-        "url": "https://ryu.clinic/welcome-to-our-new-rmt-oliviah/",
-        "teaser": "https://ryu.clinic/assets/images/oliviah.jpg"
+        "url": "/welcome-to-our-new-rmt-oliviah/",
+        "teaser": "/assets/images/oliviah.jpg"
       },{
         "title": "6 Things To Know About Massage Therapy for Health Purposes",
         "excerpt":"6 Things To Know About Massage Therapy for Health Purposes 1 The term “massage therapy” includes many techniques, and the type of massage given usually depends on your needs and physical condition. In general, massage therapists work on muscle and other soft tissue to help you feel better. A lot...","categories": [],
         "tags": ["Getting started","Treatment"],
-        "url": "https://ryu.clinic/6-things-to-know-about-massage-therapy-for-health-purposes/",
+        "url": "/6-things-to-know-about-massage-therapy-for-health-purposes/",
         "teaser": "https://source.unsplash.com/CLiwQXx7kT8"
       },{
         "title": "Massage Therapy Overview",
         "excerpt":"Massage Therapy: What You Need To Know 1 What is massage therapy used for? Massage therapy is used to help manage a health condition or enhance wellness. It involves manipulating the soft tissues of the body. Massage has been practiced in most cultures, both Eastern and Western, throughout human history,...","categories": ["Treatment"],
         "tags": ["Getting started","Treatment"],
-        "url": "https://ryu.clinic/treatment/massage-therapy-overview/",
+        "url": "/treatment/massage-therapy-overview/",
         "teaser": "https://source.unsplash.com/CLiwQXx7kT8"
       }]
